44c272055516e17f7d07dc0e284441804d9cb054
dantonifa <davidagape@hotmail.com>
2026-09-17 10:04:44 -0600

    Initial commit for contacts project

---
+++ index.js
@@ -0,0 +1,27 @@
+require("dotenv").config(); // <-- ADD THIS AS LINE 1
+require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongodb = require("./config/connect");
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+  })
+  .use("/", require("./routes"));
+
+// Initialize the database connection before starting the server
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on port ${port}`);
+  }
+});
